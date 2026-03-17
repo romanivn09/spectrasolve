53c00460726557e142b284c32693313ea2787e1b
romanivn09 <roman.ivn09@gmail.com>
2026-03-17 22:34:08 +0000

    setup autodeployment 2

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-const { onRequest } = require("firebase-functions/v2/https");
-
-// Entry point for all /api/** requests (routed via firebase.json rewrites)
-exports.api = onRequest((req, res) => {
-  res.json({ status: "ok" });
-});
